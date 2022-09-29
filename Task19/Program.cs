37596f7d6323dff23a9bc9ef958245242a1d2e5d
Twelve <v.zelensky24@gmail.com>
2022-09-29 12:16:15 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Задача 19");
+Console.Write("Введите число ");
+
+string? number = Console.ReadLine();
+
+void Checkingnumber(string number){
+    if (number[0] == number [4] || number[1] == number[3]){
+        Console.WriteLine("Это число палиндром.");
+    }
+    else Console.WriteLine("Это число не палидром.");
+}
+
+if (number!.Length == 5){
+    Checkingnumber(number);
+}
+else Console.WriteLine("Введите правильное число");
