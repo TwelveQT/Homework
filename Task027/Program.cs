3f61d9480888cfd707b879784bb7d61567cb612e
Twelve <v.zelensky24@gmail.com>
2022-10-10 23:16:47 +0300

    Task027

---
+++ Task027/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.WriteLine("Задача 27. Выдаёт сумму цифр в числе");
+
+
+Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNum(int numN){
+    
+    int counter = Convert.ToString(numN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numN - numN % 10;
+      result = result + (numN - advance);
+      numN = numN / 10;
+    }
+   return result;
+  }
+
+int sumNum = SumNum(numN);
+Console.WriteLine("Сумма цифр в числе: " + sumNum);
