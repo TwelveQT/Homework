08e1723c97e07ab5ec97c8197ea305eba3e7779d
Twelve <v.zelensky24@gmail.com>
2022-10-27 13:24:43 +0300

    Task047

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Задача 13 ");
 Console.Write("Введите число: ");
 int anyNumber = Convert.ToInt32(Console.ReadLine());
 string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2){
+if (anyNumberText.Length > 2){ 
   Console.WriteLine("третья цифра -> " + anyNumberText[2]);
 }
 else {
